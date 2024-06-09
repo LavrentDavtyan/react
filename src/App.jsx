4ff17b5fd8ff0732d571098d58201f361142d7e0
Lavrent Davtyan <lavrentdavtyan1@gmail.com>
2024-06-09 21:44:31 +0400

    implimented slider

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,99 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+    // const [photos, setPhotos] = useState([
+    //     {
+    //       id: 101,
+    //       url: "https://wallpapers.com/images/featured/prague-l8aujv2epf7ojy0r.jpg",
+    //     },
+    //     { id: 102, url: "https://images8.alphacoders.com/374/374028.jpg" },
+    //     {
+    //       id: 103,
+    //       url: "https://images7.alphacoders.com/487/thumb-1920-487159.jpg",
+    //     },
+    //     {
+    //       id: 104,
+    //       url: "https://wallpapers.com/images/hd/prague-church-at-dawn-500ebpx4fnhzqxni.jpg",
+    //     },
+    //     {
+    //       id: 105,
+    //       url: "https://wallpapers.com/images/hd/prague-churches-and-spires-kr2ll9pmha86r9tt.jpg",
+    //     },
+    //     {
+    //       id: 106,
+    //       url: "https://c4.wallpaperflare.com/wallpaper/150/315/757/prague-czechia-czech-republic-europe-wallpaper-preview.jpg",
+    //     },
+    //     { id: 107, url: "https://wallpapers-hub.art/wallpaper-images/41279.jpg" },
+    //     { id: 108, url: "https://wallpaper.dog/large/20388524.jpg" },
+    // ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      const photos = [
+          {
+            id: 101,
+            url: "https://wallpapers.com/images/featured/prague-l8aujv2epf7ojy0r.jpg",
+          },
+          { id: 102, url: "https://images8.alphacoders.com/374/374028.jpg" },
+          {
+            id: 103,
+            url: "https://images7.alphacoders.com/487/thumb-1920-487159.jpg",
+          },
+          {
+            id: 104,
+            url: "https://wallpapers.com/images/hd/prague-church-at-dawn-500ebpx4fnhzqxni.jpg",
+          },
+          {
+            id: 105,
+            url: "https://wallpapers.com/images/hd/prague-churches-and-spires-kr2ll9pmha86r9tt.jpg",
+          },
+          {
+            id: 106,
+            url: "https://c4.wallpaperflare.com/wallpaper/150/315/757/prague-czechia-czech-republic-europe-wallpaper-preview.jpg",
+          },
+          { id: 107, url: "https://wallpapers-hub.art/wallpaper-images/41279.jpg" },
+          { id: 108, url: "https://wallpaper.dog/large/20388524.jpg" },
+    ];
+
+    const[index, setIndex] = useState(0)
+
+    const handelNext = ()=>{
+        if(index >= photos.length-1){
+            setIndex(0)
+        }else{
+            setIndex(index+1)
+        }
+    }
+
+    const handelPrev = ()=>{
+        if(index > 0){
+            setIndex(index-1)
+        }else{
+            setIndex(photos.length-1)
+        }
+    }
+
+    const handelThis = (i)=>{
+        setIndex(i)
+    }
+
+    return (
+        <>
+            <img id="main" src={photos[index].url}/>
+            <div>
+                <button onClick={handelPrev}>prev</button>
+                <button onClick={handelNext}>next</button>
+            </div>
+            <div id="timeline">
+              {
+                  photos.map( (elm, i) => {
+                      const style = i == index ? "active": ""
+                      return <img src={elm.url} key={elm.id} onClick={()=> handelThis(i)} className={style}/>
+                  })
+              }
+            </div>
+        </>
+    )
 }
 
 export default App
