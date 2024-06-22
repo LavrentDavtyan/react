d6b830487d8cd578ee0ff860cbc07b459988e113
Lavrent Davtyan <lavrentdavtyan1@gmail.com>
2024-06-22 09:16:00 +0400

    project init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { AddUser } from './components/AddUser'
+import { UserList } from './components/UserList'
+import axios from 'axios'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [users, setUsers] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    useEffect(()=>{
+        axios
+        .get("http://localhost:3004/users")
+        .then(res=>{
+          // console.log(res.data)
+          setUsers(res.data)
+        })
+    },[])
+
+    const addItem = obj => {
+      setUsers([...users, obj])
+      toast.success("user has been added")
+    }
+
+    return (
+      <div className='row'>
+        <ToastContainer/>
+        <AddUser
+          onAdd = {addItem}
+        />
+        <UserList
+          users = {users}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
 export default App
